35886b2861568c57d5e21472e1746a78fd2a9181
taman01 <tomaevgenij@yandex.ru>
2022-05-19 18:31:32 +0300

    Initial commit

---
+++ src/ru/lvlup/homework/lesson2/CheckNumber.java
@@ -0,0 +1,16 @@
+package ru.lvlup.homework.lesson2;
+
+import java.util.Random;
+
+public class CheckNumber {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int secretNumber = r.nextInt(151) + 5;
+
+        if ((secretNumber > 25) && (secretNumber < 100)){
+            System.out.println("Число " + secretNumber + " содержится в интервале (25,100)");
+        }else {
+            System.out.println("Число " + secretNumber + " не содержится в интервале (25,100)");
+        }
+    }
+}
